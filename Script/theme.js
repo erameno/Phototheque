15be3285e513bc8a5c4e84ef8508c5296aa5bb08
Mael Conan Lemarchand <mllmrchnd@gmail.com>
2022-09-30 17:13:51 +0200

    Suppr pages inutiles

--- Script/theme.js
+++ Script/theme.js
@@ -4,6 +4,7 @@ var main = document.getElementById("main");
 var footer = document.getElementById("footer");
 var body = document.getElementById("body");
 var header = document.getElementById("header");
+var a = document.getElementById("a");
 
 var theme = localStorage.getItem('theme');
 if(theme == 'Dark'){
@@ -16,6 +17,7 @@ else{
 function Dark(){
 
     header.className = 'headerDark';
+    a.style.color = 'white';
     main.className = 'bodyDark';
     footer.className = 'bodyDark';
     body.className = 'bodyDark';
@@ -29,6 +31,7 @@ function Dark(){
 function White(){
     
     header.className = 'headerWhite';
+    a.style.color = 'black';
     main.className = 'bodyWhite';
     footer.className = 'bodyWhite';
     body.className = 'bodyWhite';
