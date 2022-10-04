132ba197c0d936ecec067462a491288341060289
Mael Conan Lemarchand <mllmrchnd@gmail.com>
2022-10-04 14:29:01 +0200

    Ajout image Full

--- Script/scriptPage.js
+++ Script/scriptPage.js
@@ -1,5 +1,7 @@
 let img = [...document.getElementsByClassName("imgC")];
-var divInfo = document.getElementById("divInfo");
+var divImgFull = document.getElementById("divImgFull");
+var imgFull = document.getElementById("imgFull");
+
 var btnR = document.getElementById("btnReturn");
 var btnDownload = document.getElementById("btnDel");
 var ADel = document.getElementById("ADel");
@@ -10,42 +12,27 @@ for (let i = 0; i < img.length; i++) {
 
 function Full(e){
     let imgC = e.target.closest(".img");
-    console.log(imgC);
-
-    for (let i = 0; i < img.length; i++) {
-        img[i].classList.add("imgHide");
-    };
-
-    if(imgC.classList.contains("imgHori"))
-    {
-        imgC.classList.add("imgHoriFull");
-    }
-    else
-    {
-        imgC.classList.add("imgVertiFull");
-    }
+    console.log(imgC.src);
 
     imgC.classList.remove("imgHide");
-    divInfo.classList.replace("divInfoHide", "divInfoShow");
+    divImgFull.classList.replace("divImgHide", "divImgShow");
 
+    imgFull.src = imgC.src;
     ADel.href = imgC.src;
 
 }
 
 function Return(){
-    for (let i = 0; i < img.length; i++) {
-        img[i].classList.remove("imgHide");
-    };
-
-    for(let i = 0; i < img.length; i++)
-    {
-        img[i].classList.remove("imgHoriFull");
-        img[i].classList.remove("imgVertiFull");
-    }
-
-    divInfo.classList.replace("divInfoShow", "divInfoHide");
+    divImgFull.classList.replace("divImgShow", "divImgHide");
 
 }
 
+
 img.forEach((e => e.addEventListener("click", Full)));
 btnR.addEventListener("click", Return);
+
+$(document).keyup(function(e) {
+    if (e.key === "Escape") { 
+        Return();
+   }
+});
